f6626411ed309fec136c48689d605fabe7e1d570
payalghusalikar <payal.ghusalikar9@gmail.com>
2021-03-18 15:32:38 +0530

    refactor : create note component on click hide and show

--- fundoonotes/src/components/dashboard.vue
+++ fundoonotes/src/components/dashboard.vue
@@ -30,69 +30,69 @@
         </v-row>
         <v-row>
           <v-col>
-          <v-navigation-drawer class="sideNavBar" permanent hide-overlay>
-            <v-list nav dense>
-              <v-list-item-group
-                v-model="group"
-                active-class="deep-purple--text text--accent-4"
-              >
-                <v-list-item v-for="item in sideNavBar" :key="item.icon">
-                  <v-list-item-icon>
-                    <v-icon>{{ item.icon }}</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-title v-if="showIconName">
-                    <div class="icon-name">{{ item.title }}</div>
-                  </v-list-item-title>
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
-          </v-navigation-drawer>
+            <v-navigation-drawer class="sideNavBar" permanent hide-overlay>
+              <v-list nav dense>
+                <v-list-item-group
+                  v-model="group"
+                  active-class="deep-purple--text text--accent-4"
+                >
+                  <v-list-item v-for="item in sideNavBar" :key="item.icon">
+                    <v-list-item-icon>
+                      <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title v-if="showIconName">
+                      <div class="icon-name">{{ item.title }}</div>
+                    </v-list-item-title>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-navigation-drawer>
           </v-col>
-     <v-col>
-        <v-card
-          :loading="loading"
-          class="mx-auto my-12 note-card"
-         elevation="8"
-        >
-          <v-text-field
-                v-model="title"
-                placeholder="take a Note"
-              >
-                 <v-icon>mdi-checkbox-marked</v-icon>
-                  <v-icon  >mdi-brush</v-icon>
-                  <v-icon>mdi-image</v-icon>
-            
-         </v-text-field>
-       
-
-          <v-card-title>Tonight's availability</v-card-title>
-
-          <v-card-text>
-            <v-chip-group
-              v-model="selection"
-              active-class="deep-purple accent-4 white--text"
-              column
+          <v-col>
+            <v-card
+              :loading="loading"
+              class="mx-auto my-12 note-card"
+              elevation="8"
+              @click="expandCard"
             >
-              <v-chip>5:30PM</v-chip>
+              <v-text-field v-model="title" autocomplete="off" placeholder="take a Note" flat solo>
+                <template v-slot:append>
+                  <v-icon>mdi-checkbox-marked</v-icon>
+                  <v-icon>mdi-brush</v-icon>
+                  <v-icon>mdi-image</v-icon>
+                  <!--  <v-icon v-show="cardClicked == true" class="mr-5">mdi</v-icon> v-if="item.title != 'Edit labels'"-->
+                </template>
+              </v-text-field>
 
-              <v-chip>7:30PM</v-chip>
+              <v-text-field v-model="title" placeholder="take a Note" flat solo  v-if="showBottomCard" >
+                <template>
+                
+                <v-chip-group
+                  v-model="selection"
+                  active-class="deep-purple accent-4 white--text"
+                  column
+                > 
+                  <v-icon>mdi-checkbox-marked</v-icon>
+                  <v-icon>mdi-brush</v-icon>
+                  <v-icon>mdi-image</v-icon>
+                </v-chip-group>
+                 </template>
+              </v-text-field>
 
-              <v-chip>8:00PM</v-chip>
-
-              <v-chip>9:00PM</v-chip>
-            </v-chip-group>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn color="deep-purple lighten-2" text @click="reserve">
-              Reserve
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-        
-         </v-col>
+              <v-card-actions>
+                <v-row>
+                  <v-icon>mdi-bell</v-icon>
+                  <v-icon>mdi-account</v-icon>
+                  <v-icon>mdi-brush</v-icon>
+                  <v-icon>mdi-image</v-icon>
+                  <v-icon>mdi-download</v-icon>
+                  <v-spacer />
+                  <a>Close</a>
+                </v-row>
+              </v-card-actions>
+            </v-card>
+          </v-col>
         </v-row>
- 
       </v-card>
     </v-app>
   </div>
@@ -102,34 +102,37 @@
 export default {
   data: () => ({
     showIconName: true,
-
+showBottomCard:false,
     sideNavBar: [
       {
-        title: 'Note',
-        icon: 'mdi-lightbulb',
+        title: "Note",
+        icon: "mdi-lightbulb",
       },
       {
-        title: 'Reminders',
-        icon: ' mdi-bell',
+        title: "Reminders",
+        icon: " mdi-bell",
       },
 
       {
-        title: 'Edit labels',
-        icon: 'mdi-pencil',
+        title: "Edit labels",
+        icon: "mdi-pencil",
       },
       {
-        title: 'Archieve',
-        icon: 'mdi-download',
+        title: "Archieve",
+        icon: "mdi-download",
       },
       {
-        title: 'Trash',
-        icon: 'mdi-delete',
+        title: "Trash",
+        icon: "mdi-delete",
       },
     ],
   }),
   methods: {
     drawer() {
       this.showIconName = !this.showIconName;
+    },
+    expandCard() {
+      this.showBottomCard = !this.showBottomCard;
     },
   },
 };
